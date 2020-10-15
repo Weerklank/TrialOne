1e0c5b364fdcb5a6e62726fdd9c50eda5b7f0aaa
karynclarke <karynclarke@comcast.net>
2020-10-15 15:12:38 -0700

    reworked server.js with middleware to properly connect to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 //import logo from "./logo.svg";
 import "./App.css";
+import Calendar from "./Calendar"
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-        
+        <Calendar/>
         <h2>Welcome to React</h2>
       </div>
       <p className="App-intro">
--- server.js
+++ server.js
@@ -8,15 +8,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname + "/client/build")));
 }
+
+ app.use(express.static(path.join(__dirname + '/client/build')));
 
 // Define API routes here
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 app.listen(PORT, () => {
