328d91e5b3ceb69d6150150c77019989e1157efe
Weerklank <Zoe.veggie@gmail.com>
2020-10-17 12:03:53 -0700

    sorting and react translting

---
+++ client/src-init/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import Calendar from "./Calendar"
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        
+        <Calendar/>
+        <h2>MoodScape</h2>
+      </div>
+     
+    </div>
+  );
+}
+
+
+export default App;
